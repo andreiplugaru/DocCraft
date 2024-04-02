459af1ea58ec026682d48e7fcac038de84de79af
florin rotaru <florineugen271@gmail.com>
2024-04-03 00:14:15 +0300

    download-endpoint

--- app.js
+++ app.js
@@ -11,17 +11,59 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+const { Storage } = require('@google-cloud/storage');
 'use strict';
+const bucketName = "documents_database"
+const storage = new Storage();
 
 // [START gae_flex_quickstart]
 const express = require('express');
 
 const app = express();
 
+async function downloadFile(fileName, destFileName) {
+  const options = {
+    destination: destFileName,
+  };
+
+  const bucket = storage.bucket(bucketName)
+  // Downloads the file
+  await bucket.file(fileName).download(options);
+
+  console.log(
+    `gs://${bucketName}/${fileName} downloaded to ${destFileName}.`
+  );
+}
+
+
 app.get('/', (req, res) => {
   res.status(200).send('Hello, world!').end();
 });
+
+app.get('/download', async (req, res) => {
+  const { filename, destination } = req.query;
+
+  if (!filename) {
+    return res.status(400).send('Filename is required.');
+  }
+
+  const file = storage.bucket(bucketName).file(filename);
+
+  try {
+    const exists = await file.exists();
+    if (exists[0]) {
+      const destFileName = destination ? destination : `./${filename}`; 
+      await downloadFile(filename, destFileName);
+      res.status(200).send(`File ${filename} downloaded successfully to ${destFileName}.`);
+    } else {
+      res.status(404).send(`File ${filename} not found.`);
+    }
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 
 // Start the server
 const PORT = parseInt(process.env.PORT) || 8083;
@@ -30,5 +72,5 @@ app.listen(PORT, () => {
   console.log('Press Ctrl+C to quit.');
 });
 // [END gae_flex_quickstart]
-
+  
 module.exports = app;
